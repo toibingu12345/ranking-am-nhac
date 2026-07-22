05d47c67a6bf6273e0755477b97c68ded38a97b8
toibingu12345 <caolanphuong300508@gmail.com>
2026-07-23 03:11:42 +0700

    Update data.js to include image root URL

--- src/js/data.js
+++ src/js/data.js
@@ -20,4 +20,4 @@ let dataSetVersion = '';
 /**
  * Image root, will be appended to the start of every image URL.
  */
-const imageRoot = '';
+const imageRoot = 'https://toibingu12345.github.io/ranking-am-nhac/src/assets/bia-album/';
